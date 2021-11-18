102bd4e9f955605b40d71ee47bb67a679970a1bf
cheeb <theredxer@gmail.com>
2021-11-18 09:33:41 +0000

    ok

--- src/simpleMaths.test.ts
+++ src/simpleMaths.test.ts
@@ -1,9 +1,17 @@
-import { calcAreaSquare, calcAreaTriangle } from './simpleMaths'
+import {
+  calcAreaCircle,
+  calcAreaSquare,
+  calcAreaTriangle,
+} from "./simpleMaths";
 
-test('Area Square', () => {
-    expect(calcAreaSquare(4)).toBe(16);
-})
+test("Area Square", () => {
+  expect(calcAreaSquare(4)).toBe(16);
+});
 
-test('Area Triangle', () => {
-    expect(calcAreaTriangle(4, 5)).toBe(10);
-})
+test("Area Triangle", () => {
+  expect(calcAreaTriangle(4, 5)).toBe(10);
+});
+
+test("Area Circle", () => {
+  expect(calcAreaCircle(4)).toBe(50.26548245743669);
+});
--- src/simpleMaths.ts
+++ src/simpleMaths.ts
@@ -1,8 +1,11 @@
 export function calcAreaSquare(sideLength: number): number {
-    return Math.pow(sideLength, 2);
+  return Math.pow(sideLength, 2);
 }
 
 export function calcAreaTriangle(width: number, height: number): number {
-    return width * height / 2;
+  return (width * height) / 2;
 }
 
+export function calcAreaCircle(radius: number): number {
+  return Math.PI * Math.pow(radius, 2);
+}
